d6bfb807ebb6e82bcbcfa353919f1ad6a3a6d147
ogaw-s <d257bk512@gmail.com>
2025-06-19 17:20:14 +0900

    iroiro

--- tcp_stream.c
+++ tcp_stream.c
@@ -25,9 +25,9 @@ int setup_socket(int is_server, const char *ip, int port)
         }
 
         struct sockaddr_in serv_addr = {0};
-        serv_addr.sin_family      = AF_INET;
+        serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        serv_addr.sin_port        = htons(port);
+        serv_addr.sin_port = htons(port);
 
         if (bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
             perror("bind");
